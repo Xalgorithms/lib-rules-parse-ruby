178d37617129284c179852cd5d30797cd960ff39
Don Kelly <karfai@gmail.com>
2016-04-23 20:46:18 -0400

    A first attempt at :join

--- lib/xa/rules/rule.rb
+++ lib/xa/rules/rule.rb
@@ -7,19 +7,72 @@ module XA
 
       def initialize
         @meta = OpenStruct.new(expects: [])
+        @actions = []
       end
       
       def expects(table_name, column_names)
         @meta.expects << OpenStruct.new(table: table_name, columns: column_names)
       end
 
+      def use(table_name)
+        @actions << Use.new(table_name)
+        @actions.last
+      end
+
+      def apply(table_name, active_cols, joint_cols)
+        @actions << Apply.new(table_name, active_cols, joint_cols)
+        @actions.last
+      end
+
       def execute(tables)
         res = verify_expectations(tables) do
-          OpenStruct.new(status: :ok, failures: [])
+          env = {
+            active: nil,
+            tables: tables,
+          }
+          
+          @actions.each do |act|
+            env = act.execute(env)
+          end
+          OpenStruct.new(status: :ok, failures: [], tables: env[:tables])
         end
       end
 
       private
+
+      class Use
+        def initialize(name)
+          @name = name
+        end
+
+        def execute(env)
+          env.merge(active: @name)
+        end
+      end
+
+      class Apply
+        def initialize(name, active_cols, joint_cols)
+          @name = name
+          @active_cols = active_cols
+          @joint_cols = joint_cols
+        end
+
+        def using(fn)
+        end
+
+        def execute(env)
+          atbl = env[:tables].fetch(env[:active], [])
+          jtbl = env[:tables].fetch(@name, [])
+          ftbl = atbl.inject([]) do |a, ar|
+            avals = @active_cols.map { |k| ar.fetch(k, nil) }
+            jrows = jtbl.select { |jr| avals == @joint_cols.map { |k| jr.fetch(k, nil) } }
+            a + (jrows.any? ? jrows.map { |jr| ar.merge(jr) } : [ar])
+          end
+
+          env[:tables][env[:active]] = ftbl
+          env
+        end
+      end
       
       def verify_expectations(tables)
         missing = @meta.expects.select { |ex| !tables.key?(ex.table) }.map { |ex| ex.table }
--- spec/xa/rules/rule_spec.rb
+++ spec/xa/rules/rule_spec.rb
@@ -43,5 +43,48 @@ describe XA::Rules::Rule do
       expect(res.status).to eql(:ok)
       expect(res.failures).to be_empty
     end
+
+    it 'joins two tables' do
+      r = XA::Rules::Rule.new
+
+      tables = {
+        'foo' => [
+          { 'x' => 1, 'y' => 2, 'z' => 1 },
+          { 'x' => 2, 'y' => 2, 'z' => 2 },
+          { 'x' => 1, 'y' => 1, 'z' => 3 },
+          { 'x' => 3, 'y' => 3, 'z' => 4 },
+        ],
+        'bar' => [
+          { 'a' => 1, 'b' => 1, 'c' => 0 },
+          { 'a' => 2, 'b' => 2, 'c' => 1 },
+        ]
+      }
+
+      expected = [
+        {
+          table: 'foo',
+          join:  ['bar', ['x', 'y'], ['a', 'b']],
+          final: {
+            'foo' => [
+              { 'x' => 1, 'y' => 2, 'z' => 1 },
+              { 'x' => 2, 'y' => 2, 'z' => 2, 'a' => 2, 'b' => 2, 'c' => 1 },
+              { 'x' => 1, 'y' => 1, 'z' => 3, 'a' => 1, 'b' => 1, 'c' => 0 },
+              { 'x' => 3, 'y' => 3, 'z' => 4 },
+            ],
+            'bar' => [
+              { 'a' => 1, 'b' => 1, 'c' => 0 },
+              { 'a' => 2, 'b' => 2, 'c' => 1 },
+            ],
+          },
+        },
+      ]
+
+      expected.each do |ex|
+        r.use(ex[:table])
+        r.apply(*ex[:join]).using(:join)
+        res = r.execute(tables)
+        expect(res.tables).to eql(ex[:final])
+      end
+    end
   end
 end
