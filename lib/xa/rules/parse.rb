b4189a8e0c52ee00263b5211d162403294c3f3ac
Don Kelly <karfai@gmail.com>
2016-05-30 15:35:23 -0400

    Parsing of other meta data and factor

--- lib/xa/rules/parse.rb
+++ lib/xa/rules/parse.rb
@@ -5,6 +5,7 @@ module XA
     module Parse
       class ActionParser < Parslet::Parser
         rule(:comma)        { str(',') }
+        rule(:colon)        { str(':') }
         rule(:lblock)       { str('[') }
         rule(:rblock)       { str(']') }
         
@@ -15,14 +16,19 @@ module XA
         rule(:pushs)        { str('PUSH') }
         rule(:pops)         { str('POP') }
         rule(:duplicates)   { str('DUPLICATE') }
+        rule(:pulls)        { str('PULL') }
+        rule(:attachs)      { str('ATTACH') }
+        rule(:invokes)      { str('INVOKE') }
 
         rule(:names)        { name >> (comma >> space.maybe >> name).repeat }
         rule(:name)         { match('\w+').repeat(1) }
         rule(:names_as)     { name_as >> (comma >> space.maybe >> name_as).repeat }
         rule(:name_as)      { name.as(:original) >> (space >> ass >> space >> name.as(:new)).maybe }
+        rule(:anything)     { match('[^\s]').repeat(1) }
         
         rule(:table_ref)      { name.as(:table_name) >> lblock >> names.as(:columns) >> rblock }
         rule(:table_ref_opt)  { name.as(:table_name) >> (lblock >> names.as(:columns) >> rblock).maybe }
+        rule(:rule_ref)       { name.as(:repo) >> colon >> name.as(:rule) >> colon >> name.as(:version) }
         rule(:join_spec)      { lblock >> lblock >> names.as(:lefts) >> rblock >> comma >> space.maybe >> lblock >> names.as(:rights) >> rblock >> rblock }
         rule(:includes_spec)  { lblock >> names_as >> rblock }
         rule(:joinish)        { usings >> space >> join_spec.as(:joins) >> space >> includes >> space >> includes_spec.as(:includes) }
@@ -32,11 +38,16 @@ module XA
         rule(:push)           { pushs.as(:action) >> space >> name.as(:table_name) }
         rule(:pop)            { pops.as(:action) }
         rule(:duplicate)      { duplicates.as(:action) }
+        rule(:pull)           { pulls.as(:action) >> space >> rule_ref.as(:rule_ref) >> space >> ass >> space >> name.as(:table_name) }
+        rule(:attach)         { attachs.as(:action) >> space >> anything.as(:url) >> space >> ass >> space >> name.as(:name) }
+        rule(:invoke)         { invokes.as(:action) >> space >> rule_ref.as(:rule_ref) }
         
         rule(:table_action)   { expects | commit }
         rule(:joinish_action) { name.as(:action) >> space >> joinish }
         rule(:stack_action)   { push | pop | duplicate }
-        rule(:action)         { table_action | joinish_action | stack_action }
+        rule(:repo_action)    { attach | pull }
+        rule(:rule_action)    { invoke }
+        rule(:action)         { table_action | joinish_action | stack_action | repo_action | rule_action }
 
         root(:action)
       end
@@ -65,11 +76,19 @@ module XA
       end
 
       def interpret_expects(o, res)
-        meta = o.fetch('meta', {})
-        expects = meta.fetch('expects', {}).merge(
-          res[:table_name].str => split_names(res[:columns]))
-        meta = meta.merge('expects' => expects)
-        o.merge('meta' => meta)
+        add_meta(o, 'expects', res[:table_name].str => split_names(res[:columns]))
+      end
+
+      def interpret_pull(o, res)
+        add_meta(o, 'tables', res[:table_name].str => {
+                   'repository' => res[:rule_ref][:repo].str,
+                   'name'       => res[:rule_ref][:rule].str,
+                   'version'    => res[:rule_ref][:version].str,
+                 })
+      end
+
+      def interpret_attach(o, res)
+        add_meta(o, 'repositories', res[:name].str => res[:url].str)
       end
 
       def interpret_push(o, res)
@@ -82,6 +101,16 @@ module XA
 
       def interpret_duplicate(o, res)
         add_action(o, 'name'  => 'duplicate')
+      end
+      
+      def interpret_invoke(o, res)
+        act = {
+          'name' => 'invoke',
+          'repository' => res[:rule_ref][:repo].str,
+          'rule'       => res[:rule_ref][:rule].str,
+          'version'    => res[:rule_ref][:version].str,
+        }
+        add_action(o, act)
       end
       
       def interpret_commit(o, res)
@@ -116,6 +145,13 @@ module XA
 
       def add_action(o, act)
         o.merge('actions' => o.fetch('actions', []) << act)
+      end
+
+      def add_meta(o, section, d)
+        meta = o.fetch('meta', {})
+        sec = meta.fetch(section, {}).merge(d)
+        meta = meta.merge(section => sec)
+        o.merge('meta' => meta)
       end
       
       def parser
--- spec/xa/rules/parse_spec.rb
+++ spec/xa/rules/parse_spec.rb
@@ -22,6 +22,35 @@ describe XA::Rules::Parse do
 
       {
         in: [
+          'PULL repo0:foo:1234 AS foo0',
+          'PULL repo1:bar:3333 AS bar1',
+          'ATTACH http://www.example0.org/foo AS repo0',
+          'ATTACH http://www.example1.org/foo AS repo1',
+        ],
+        out: {
+          'meta' => {
+            'repositories' => {
+              'repo0' => 'http://www.example0.org/foo',
+              'repo1' => 'http://www.example1.org/foo',
+            },
+            'tables' => {
+              'foo0' => {
+                'repository' => 'repo0',
+                'name'       => 'foo',
+                'version'    => '1234',
+              },
+              'bar1' => {
+                'repository' => 'repo1',
+                'name'       => 'bar',
+                'version'    => '3333',
+              },
+            },
+          }
+        },
+      },
+
+      {
+        in: [
           'COMMIT foo[a, b]',
           'COMMIT bar[p, q, r]',
           'COMMIT baz',
@@ -69,6 +98,29 @@ describe XA::Rules::Parse do
               'name'    => 'push',
               'table'   => 'bar',
              },
+          ],
+        },
+      },
+
+      {
+        in: [
+          'INVOKE repo0:name0:1111',
+          'INVOKE repo1:name2:3333',
+        ],
+        out: {
+          'actions' => [
+            {
+              'name'       => 'invoke',
+              'repository' => 'repo0',
+              'rule'       => 'name0',
+              'version'    => '1111',
+            },
+            {
+              'name'       => 'invoke',
+              'repository' => 'repo1',
+              'rule'       => 'name2',
+              'version'    => '3333',
+            },
           ],
         },
       },
