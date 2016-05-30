c06875d45fe327f0664ea9a0f714fbfaed1084ac
Don Kelly <karfai@gmail.com>
2016-05-30 01:39:48 -0400

    Extend parser to support joinish statements

--- lib/xa/rules/parse.rb
+++ lib/xa/rules/parse.rb
@@ -9,13 +9,23 @@ module XA
         rule(:rblock)       { str(']') }
         
         rule(:space)        { match('\s').repeat(1) }
+        rule(:ass)          { str('AS') }
+        rule(:usings)       { str('USING') }
+        rule(:includes)     { str('INCLUDE') }
 
         rule(:names)        { name >> (comma >> space.maybe >> name).repeat }
         rule(:name)         { match('[a-zA-Z]').repeat(1) }
-        rule(:table_ref)    { name.as(:table_name) >> lblock >> names.as(:columns) >> rblock }
+        rule(:names_as)     { name_as >> (comma >> space.maybe >> name_as).repeat }
+        rule(:name_as)      { name.as(:original) >> (space >> ass >> space >> name.as(:new)).maybe }
+        
+        rule(:table_ref)      { name.as(:table_name) >> lblock >> names.as(:columns) >> rblock }
+        rule(:join_spec)      { lblock >> lblock >> names.as(:lefts) >> rblock >> comma >> space.maybe >> lblock >> names.as(:rights) >> rblock >> rblock }
+        rule(:includes_spec)  { lblock >> names_as >> rblock }
+        rule(:joinish)        { usings >> space >> join_spec.as(:joins) >> space >> includes >> space >> includes_spec.as(:includes) }
 
-        rule(:table_action) { name.as(:action) >> space.repeat(1) >> table_ref  }
-        rule(:action)       { table_action }
+        rule(:table_action)   { name.as(:action) >> space >> table_ref  }
+        rule(:joinish_action) { name.as(:action) >> space >> joinish }
+        rule(:action)         { table_action | joinish_action }
 
         root(:action)
       end
@@ -35,6 +45,10 @@ module XA
 
       private
 
+      def split_names(names)
+        names.str.split(/\,\s+/)
+      end
+      
       def interpret(o, res)
         send("interpret_#{res.fetch(:action, 'nothing').str.downcase}", o, res)
       end
@@ -42,11 +56,34 @@ module XA
       def interpret_expects(o, res)
         meta = o.fetch('meta', {})
         expects = meta.fetch('expects', {}).merge(
-          res[:table_name].str => res[:columns].str.split(/\,\s+/))
+          res[:table_name].str => split_names(res[:columns]))
         meta = meta.merge('expects' => expects)
         o.merge('meta' => meta)
       end
+
+      def interpret_join(o, res)
+        interpret_joinish(o, res)
+      end
+
+      def interpret_inclusion(o, res)
+        interpret_joinish(o, res)
+      end
       
+      def interpret_joinish(o, res)
+        actions = o.fetch('actions', [])
+        actions << {
+          'name'    => res[:action].str.downcase,
+          'using'   => {
+            'left'  =>  split_names(res[:joins][:lefts]),
+            'right' => split_names(res[:joins][:rights]),
+          },
+          'include' => res[:includes].inject({}) do |o, i|
+            o.merge(i[:original].str => i.key?(:new) ? i[:new].str : i[:original].str)
+          end
+        }
+        o.merge('actions' => actions)
+      end
+
       def parser
         @parser ||= ActionParser.new
       end
--- spec/xa/rules/parse_spec.rb
+++ spec/xa/rules/parse_spec.rb
@@ -3,7 +3,7 @@ require 'xa/rules/parse'
 describe XA::Rules::Parse do
   include XA::Rules::Parse
 
-  it 'parses expect statements' do
+  it 'parses' do
     expectations = [
       {
         in: [
@@ -19,6 +19,39 @@ describe XA::Rules::Parse do
           }
         },
       },
+
+      {
+        in: [
+          'JOIN USING [[a, b], [x, y]] INCLUDE [p AS pp, q]',
+          'INCLUSION USING [[r, s], [zz, yy]] INCLUDE [r AS rr, s AS ss]',
+        ],
+        out: {
+          'actions' => [
+            {
+              'name' => 'join',
+              'using' => {
+                'left'  => ['a', 'b'],
+                'right' => ['x', 'y'],
+              },
+              'include' => {
+                'p' => 'pp',
+                'q' => 'q',
+              },
+            },
+            {
+              'name' => 'inclusion',
+              'using' => {
+                'left'  => ['r', 's'],
+                'right' => ['zz', 'yy'],
+              },
+              'include' => {
+                'r' => 'rr',
+                's' => 'ss',
+              },
+            },
+          ],
+        },
+      }
     ]
 
     expectations.each do |ex|
