5e0fbe2a1deec520b26f8214d8e4ff345d19aa55
Don Kelly <karfai@gmail.com>
2018-03-30 02:23:09 +0400

    Reorganise keyword tests
    Change-Id: Idef93a13726617f0909fc11698804b7a0a127ae4

--- spec/xa/rules/parse_spec.rb
+++ spec/xa/rules/parse_spec.rb
@@ -1,23 +1,21 @@
+require 'multi_json'
 require 'radish/expects'
-
 require 'xa/rules/parse'
 
 describe XA::Rules::Parse do
   include XA::Rules::Parse
   include Radish::Expects
 
-  it 'should parse the syntax' do
+  it 'should parse individual keyword syntax' do
     files = [
-      'spec/files/when.json',
-      'spec/files/require.json',
-      'spec/files/assemble.json',
-      'spec/files/keep.json',
-      'spec/files/map.json',
-      'spec/files/revise.json',
-      'spec/files/whitespace.json'
-#      'spec/files/mixed.json',
-    ]
-
+      'assemble',
+      'keep',
+      'map',
+      'require',
+      'revise',
+      'when',
+      'whitespace',
+    ].map { |n| "spec/files/keywords/#{n}.json" }
     load_expects(files, method(:parse)) do |ex, ac|
       expect(ac).to eql(ex)
     end
