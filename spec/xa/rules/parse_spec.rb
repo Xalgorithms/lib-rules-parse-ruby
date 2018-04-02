f617b0154edad5544dd32cfb363c59e4ae97e216
Don Kelly <karfai@gmail.com>
2018-04-02 04:03:57 +0400

    Reformulate the specs to get better output and failures
    Change-Id: I1cc2de36cf0a716e3f6adc52286f95b76dde7cb1

--- spec/xa/rules/parse_spec.rb
+++ spec/xa/rules/parse_spec.rb
@@ -6,26 +6,26 @@ describe XA::Rules::Parse do
   include XA::Rules::Parse
   include Radish::Expects
 
-  it 'should parse individual keyword syntax' do
-    files = [
-      'assemble',
-      'filter',
-      'keep',
-      'map',
-      'require',
-      'revise',
-      'when',
-      'whitespace',
-    ].map { |n| "spec/files/keywords/#{n}.json" }
-    load_expects(files, method(:parse)) do |ex, ac|
-      expect(ac).to eql(ex)
+  [
+    'assemble',
+    'filter',
+    'keep',
+    'map',
+    'require',
+    'revise',
+    'when',
+    'whitespace',
+  ].each do |n|
+    it "should parse syntax for #{n.upcase}" do
+      load_expects(["spec/files/keywords/#{n}.json"], method(:parse)) do |ex, ac|
+        expect(ac).to eql(ex)
+      end
     end
   end
 
-  it 'should parse complete rule files' do
-    files = Dir.glob('spec/files/rules/*.rule')
-    files.each do |ffn|
-      (dn, fn) = File.split(ffn)
+  Dir.glob('spec/files/rules/*.rule').each do |ffn|
+    (dn, fn) = File.split(ffn)
+    it "should parse: #{fn}" do
       ex = MultiJson.decode(File.read(File.join(dn, "#{File.basename(ffn, '.rule')}.json")))
       expect(parse(IO.read(ffn))).to eql(ex)
     end
